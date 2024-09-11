a487a79926dc5ccd34a83b599ccbb0b06393d306
Steven Stuart <w8an@users.noreply.github.com>
2024-09-11 17:31:29 -0400

    Code cleanup and comments

--- src/esp32-stream-system.cpp
+++ src/esp32-stream-system.cpp
@@ -1,21 +1,16 @@
 /**
  * esp32-stream-system.cpp
- * jul-aug 2024
+ * jul-sep 2024
  * 
  * Copyright (C)2024, Steven R Stuart
  * 
- * This program outputs url stream to max98357a i2c audio device(s).
+ * This program outputs mp3 internet stream to max98357a i2c audio device(s).
  * Turn the knob to set volume. Press the button to change station.
  * Set volume to 0 then press button to set timer. Do again to cancel.
- * Hold pin 15 low on reset to launch configuration portal.
+ * Two clicks within 3 seconds with zero volume halts stream.
+ * Hold pin 15 low on reset to launch wifi configuration portal.
  * Hold pin 16 low on reset to store stream data from presets.
  */
-
-/*
-  Stream tag (name) and url are stored in the preferences system (prefs)
-    for off-line data persistence.
-  The system uses the streamsX array when running.
-*/
 
 #include <Arduino.h>
 #include <Preferences.h>
@@ -32,26 +27,25 @@
 #define ROTARY_ENCODER_A_PIN 33      // clk
 #define ROTARY_ENCODER_B_PIN 32      // dt  
 #define ROTARY_ENCODER_BUTTON_PIN 34 // sw
-#define ROTARY_ENCODER_STEPS 2
+#define ROTARY_ENCODER_STEPS 2       // 1, 2 or 4
 
 #define I2C_ADDRESS 0x3C  // ssd1306 oled
 #define SDA_PIN 18        // i2c data
 #define SCL_PIN 19        // i2c clock
 
+#define PORTAL_PIN 15     // enable wifi portal when low on reset      
+#define STREAM_PIN 16     // set default streams when low on reset
+
 #define OLED_TIMER 5000   // 5 seconds, display timeout
-#define STREAM_TIMER 2000 // 2 seconds, audio wait time after selection    // // deprecate
 
-#define PORTAL_PIN 15     // wifi portal when low on reset      
-#define STREAM_PIN 16     // set default streams when held low on reset
-
-// System is hard coded to 25 streams
-// Each item in the streamsX array contains a text name and a url
-// The name and url elements are each STREAM_ELEMENT_SIZE in length
+// System is hard coded to 25 streams (TOTAL_ITEMS)
+// Each item in the streamsX array contains a text name and a url,
+// the name and url elements are each STREAM_ELEMENT_SIZE in length
 #define STREAM_ITEM_SIZE 100          // line item width
 #define STREAM_ELEMENT_SIZE 50        // max length of name, url
-const int totalItems = 25;            // number of line items in streamsX
-char streamsX[2500];                  // STREAM_ITEM_SIZE * totalItems
+#define TOTAL_ITEMS 25                // number of line items in streamsX
 int currentIndex = 0;                 // stream pointer 
+char streamsX[TOTAL_ITEMS * STREAM_ITEM_SIZE]; // names & urls of stations
 
 const char* portalName = "NETRADIO";  // portal ssid
 const char* settings = "settings";    // general purpose namespace in prefs
@@ -59,11 +53,11 @@ const char* listened = "listened";    // settings key of last listened to stream
 const char* audiovol = "volume";      // settings key of audio level
 
 AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(
-    ROTARY_ENCODER_A_PIN, 
-    ROTARY_ENCODER_B_PIN, 
-    ROTARY_ENCODER_BUTTON_PIN, 
-    -1,                          // Vcc pin, -1 = not used
-    ROTARY_ENCODER_STEPS
+    ROTARY_ENCODER_A_PIN,        // ky040 clk
+    ROTARY_ENCODER_B_PIN,        //   "   dt
+    ROTARY_ENCODER_BUTTON_PIN,   //   "   sw
+    -1,                          // Vcc pin -1 = not used
+    ROTARY_ENCODER_STEPS         
 );
 
 void IRAM_ATTR readEncoderISR() {  // interrupt service routine
@@ -79,25 +73,30 @@ StreamCopy copier(mp3decode, urlstream); // copy urlstream to decoder
 SSD1306AsciiWire oled;
 Preferences prefs;              // persistent data store
 
-char stream_item[25][2] = {     // prefs stream item names
+char stream_item[TOTAL_ITEMS][2] = {     // prefs stream item names
   "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
-  "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "R"};
+  "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", 
+  "L", "M", "N", "P", "R"
+};
 char stream_type[2][4] = {"tag", "url"}; // prefs key names
+#define TYPE_TAG 0  // Type references
+#define TYPE_URL 1  
 
+// Must have enough unique element names for TOTAL_ITEMS
 const char* tagElement[] = {    // portal html element tags
   "tag0",  "tag1",  "tag2",  "tag3",  "tag4", 
   "tag5",  "tag6",  "tag7",  "tag8",  "tag9", 
   "tag10", "tag11", "tag12", "tag13", "tag14", 
   "tag15", "tag16", "tag17", "tag18", "tag19",
   "tag20", "tag21", "tag22", "tag23", "tag24"
-  };
+};
 const char* nameElement[] = {   // portal html tag field titles
   "Name 1",  "Name 2",  "Name 3",  "Name 4",  "Name 5",  
   "Name 6",  "Name 7",  "Name 8",  "Name 9",  "Name 10", 
   "Name 11", "Name 12", "Name 13", "Name 14", "Name 15", 
   "Name 16", "Name 17", "Name 18", "Name 19", "Name 20", 
   "Name 21", "Name 22", "Name 23", "Name 24", "Name 25"
-  };
+};
 const char* urlElement[] = {    // portal url element tags/titles
   "URL_1",  "URL_2",  "URL_3",  "URL_4",  "URL_5",  
   "URL_6",  "URL_7",  "URL_8",  "URL_9",  "URL_10", 
@@ -105,10 +104,6 @@ const char* urlElement[] = {    // portal url element tags/titles
   "URL_16", "URL_17", "URL_18", "URL_19", "URL_20", 
   "URL_21", "URL_22", "URL_23", "URL_24", "URL_25"
 };
-
-// Types for the array streamsX[] and the preference stream_type[] 
-#define TYPE_TAG 0
-#define TYPE_URL 1
 
 // Function prototypes
 void populateStreams(void);
@@ -132,12 +127,12 @@ String timerTimeLeft(void);
  */
 void setup() {
 
-  pinMode(PORTAL_PIN, INPUT_PULLUP);  // call for portal
-  pinMode(STREAM_PIN, INPUT_PULLUP);  // default stream set up
+  pinMode(PORTAL_PIN, INPUT_PULLUP);  // call for wifi portal
+  pinMode(STREAM_PIN, INPUT_PULLUP);  // load default streams
 
   // Message port
   Serial.begin(115200);
-  Serial.println(F("--[ Stream Player ]--"));
+  Serial.println(F("Stream Player"));
   Serial.print(F("Steven R Stuart,  "));
   Serial.print(F(__DATE__));
   Serial.print(F(" "));
@@ -149,17 +144,19 @@ void setup() {
   oled.begin(&Adafruit128x32, I2C_ADDRESS);  // start oled
   oled.setFont(System5x7);
   oled.clear();
-  oled.print(F("Stream Player\nSteven R Stuart"));
+  oled.print(F("Stream Player\nSteven R Stuart\nW8AN\n"));
+  //oled.print(F(__DATE__));
+  //oled.print(F(" "));
+  //oled.print(F(__TIME__));
 
   // Reload the default streams if desired
   if (digitalRead(STREAM_PIN) == LOW) {
-    initializeStreams(); // get the defaults
+    initializeStreams(); // get the default streams
   }
-
   populateStreams();  // fill streamsX array
   currentIndex = settingGet(listened);  // get index of prev listened stream
 
-  // Wifi
+  // Configure Wifi system
   WiFiManager wifiMan;
   wifiMan.setDebugOutput(false);  // true if you want send to serial debug
   // wifiMan.resetSettings();     // force a wifi configuration portal
@@ -183,9 +180,9 @@ void setup() {
       }
 
       //  set up portal parameters
-      WiFiManagerParameter* tagElementParam[25]; // Array to store pointers to WiFiManagerParameter tag objects
-      WiFiManagerParameter* urlElementParam[25]; // and WiFiManagerParameter url objects
-      for (int i=0; i<25; i++) {
+      WiFiManagerParameter* tagElementParam[TOTAL_ITEMS]; // Array to store pointers to WiFiManagerParameter tag objects
+      WiFiManagerParameter* urlElementParam[TOTAL_ITEMS]; // and WiFiManagerParameter url objects
+      for (int i=0; i<TOTAL_ITEMS; i++) {
         tagElementParam[i] = new WiFiManagerParameter{tagElement[i], nameElement[i], streamsGetTag(i), STREAM_ELEMENT_SIZE-1};
         wifiMan.addParameter(tagElementParam[i]);
         urlElementParam[i] = new WiFiManagerParameter{urlElement[i], urlElement[i], streamsGetUrl(i), STREAM_ELEMENT_SIZE-1};
@@ -197,10 +194,10 @@ void setup() {
       oled.print(portalName);
       oled.print(F("\nIP  : 192.168.4.1"));
 
-      wifiMan.startConfigPortal(portalName); 
+      wifiMan.startConfigPortal(portalName); // get user data from wifi connected device
 
       // Store data fields from portal into streamsX array
-      for (int i=0; i<25; i++) {
+      for (int i=0; i<TOTAL_ITEMS; i++) {
         streamsPut(i, tagElementParam[i]->getValue(), urlElementParam[i]->getValue()); 
       }
 
@@ -224,7 +221,7 @@ void setup() {
   rotaryEncoder.setAcceleration(25);
   //rotaryEncoder.disableAcceleration();
 
-  // Audio error messages
+  // Audio system error messages
   AudioLogger::instance().begin(Serial, AudioLogger::Warning);  // Debug, Info, Warning, Error
 
   // Output stream configuration
@@ -276,9 +273,12 @@ unsigned long sleepTimer;
  */
 void loop() {
   if (systemSleeping) {
+    
     timerRunning = false;
+    
     if (rotaryEncoder.isEncoderButtonClicked() ||
         rotaryEncoder.encoderChanged()) {
+
       // wake from sleep
       systemSleeping = false;
 
@@ -293,78 +293,96 @@ void loop() {
     if (systemStreaming) copier.copy();  // Run the open audio stream
     
     else {  // start a stream
+
       if (checkProtocol(currentIndex)) {
+        // url seems ok
         urlstream.begin(streamsGetUrl(currentIndex));
         settingPut(listened, currentIndex);  // save selection
         systemStreaming = true;
       }
-      else { // url string length too short
+
+      else { 
+        // url string length is too short
         currentIndex = settingGet(listened); // get previous stream
         menuIndex = currentIndex;  // reset menu display pointer
         oled.clear();
-        oled.println("ERROR");
+        oled.println("ERROR");     // notify user
         oled.println("Missing URL");
         oled.println();
         oled.print("Reverting");
       }
+
       displayOn = true;
       oledStartTime = millis(); // tickle the display timer
     }
 
     if (rotaryEncoder.isEncoderButtonClicked()) { 
-      if (settingGet(audiovol) == 0) { 
-        // set/reset sleep timer or power down when volume is zero
-        oled.clear();
+      // button was pressed
 
+      if (settingGet(audiovol) == 0) { 
+        // volume is zero
+
+        // set\reset sleep timer or power down
+        oled.clear();
         sleepCurrentTime = millis();
-        if (timerRunning & ((sleepCurrentTime - sleepStartTime) < 3000)) {
+        if (timerRunning & (sleepCurrentTime - sleepStartTime) < 3000) {
           // if user presses button within 3 seconds of timer set
           // and volume is zero, stop audio streaming
-          systemSleeping = true;
+          urlstream.end();          // stop stream download
+          systemStreaming = false;  // set state signals
           timerRunning = false;
-          urlstream.end();
-          systemStreaming = false;
-          oled.println("SYSTEM HALT");
+          systemSleeping = true;
+          oled.println("SYSTEM HALT"); // notify user
         }
+
         else {
           // Set or reset sleep timer when volume is zero
-          setSleepTime();  // set/reset timer minutes
+          setSleepTime();  // set\reset timer milliseconds
 
-          if (sleepTimer == 0) {
+          if (sleepTimer == 0) { 
             // cancel timer
             timerRunning = false;
             oled.println("CANCEL TIMER");
-          } 
-          else {
+          }
+
+          else { 
+            // timer has been set
             timerRunning = true;
             oled.println("TIMER SET"); 
             oled.println("60 minutes");  
           }
         }
+
         displayOn = true;
         oledStartTime = millis(); // tickle the display timer
       }
+
       else {
-        if (menuOpen) { // close the menu
+        if (menuOpen) { 
+          // close the menu, prepare selected stream
           menuOpen = false;
           rotaryEncoder.setEncoderValue(volumePos);   // restore volume position 
           currentIndex = menuIndex;  // user chose this stream
-          urlstream.end();
+          urlstream.end();           // stop stream download
           systemStreaming = false;   // signal that another stream is selected
           oledStatusDisplay();
         }
-        else { // open the menu, select a stream
+
+        else { 
+          // open the menu, select a stream
           menuOpen = true;
           volumePos = rotaryEncoder.readEncoder(); // get volume setting
           rotaryEncoder.setEncoderValue(50);       // initialize position
-          menuDisplay(currentIndex);
+          menuDisplay(currentIndex);  // show stream selection list
         }
       }
+
       displayOn = true;
       oledStartTime = millis(); // tickle the display timer
     }
   
     if (rotaryEncoder.encoderChanged()) {   // knob has turned
+
       if (menuOpen) {  
         // scroll the menu
         long encoderCurrPos = rotaryEncoder.readEncoder();
@@ -372,17 +390,20 @@ void loop() {
         else menuIndex++;
         rotaryEncoder.setEncoderValue(50); // re-initialize position
 
-        if (menuIndex >= (totalItems-1)) menuIndex = 0; // wrap around
-        if (menuIndex < 0) menuIndex = totalItems-1;
+        if (menuIndex >= (TOTAL_ITEMS-1)) menuIndex = 0; // wrap around
+        if (menuIndex < 0) menuIndex = TOTAL_ITEMS-1;
 
         menuDisplay(menuIndex);  // show stream selection list
       }
-      else { // menu is not open, set volume
+
+      else { 
+        // menu is not open, so set the volume level
         volLevel = 100 - rotaryEncoder.readEncoder();
         volume.setVolume(volLevel / 100.0);
         settingPut(audiovol, volLevel);  // store the setting
         oledStatusDisplay();  
       }
+
       displayOn = true;
       oledStartTime = millis(); // tickle the display timer
     }
@@ -391,25 +412,33 @@ void loop() {
   // Turn off oled after a few seconds
   oledCurrentTime = millis();
   if (displayOn) {
+
     if (oledCurrentTime - oledStartTime > OLED_TIMER) {
+
+      // oled timer has expired, turn it off
       if (menuOpen) {
         menuOpen = false;
-        menuIndex = currentIndex;  // reset menu display pointer
+        menuIndex = currentIndex;  // no selection, reset menu display pointer
         rotaryEncoder.setEncoderValue(volumePos);   // restore volume position 
       }
+
       oled.clear();  // blank the display
       displayOn = false;
     }
   }
 
-  if (!systemSleeping && timerRunning) { // awake and timer is running
+  if (!systemSleeping && timerRunning) { 
+    // system is awake and timer is running
+    
+    // watch for timeout
     sleepCurrentTime = millis();
     if (sleepCurrentTime - sleepStartTime > sleepTimer) {
-      // time to go to sleep
-      systemSleeping = true;
+
+      // timer has expired, go to sleep
+      urlstream.end();         // stop stream download
+      systemStreaming = false; // set state signals
       timerRunning = false;
-      urlstream.end();
-      systemStreaming = false;
+      systemSleeping = true;
 
       oled.clear();
       oled.println("SLEEPING");
@@ -486,29 +515,29 @@ void menuDisplay(int menuIndex) {
   oled.println(streamsGetTag(currentIndex));  // title line
 
   // previous line item
-  int lineIndex = (menuIndex == 0 ? (totalItems-1) : menuIndex-1);
-  oled.print(" ");
+  int lineIndex = (menuIndex == 0 ? (TOTAL_ITEMS-1) : menuIndex-1);
+  oled.print(" "); // indent
   if (checkProtocol(lineIndex)) oled.println(streamsGetTag(lineIndex));
-  else oled.println(lineIndex+1);
+  else oled.println(lineIndex+1); // print only line number if error
   
   // current line item
   if (checkProtocol(menuIndex)) {  // selection line
-    oled.print(">");
+    oled.print(">"); // current item pointer
     oled.println(streamsGetTag(menuIndex));
   } 
   else {
-    oled.print(" ");
+    oled.print(" "); // indent
     oled.println(menuIndex+1);
   }
   
   // next line item
-  lineIndex = (menuIndex == (totalItems-1) ? 0 : menuIndex+1);
-  oled.print(" ");
+  lineIndex = (menuIndex == (TOTAL_ITEMS-1) ? 0 : menuIndex+1);
+  oled.print(" "); // indent
   if (checkProtocol(lineIndex)) oled.println(streamsGetTag(lineIndex));
   else oled.print(lineIndex+1);
 }
 
-// Return true if the url protocol is correct
+// Return true if the url protocol text is correct
 bool checkProtocol(int menuIndex) {
   return strncmp(streamsGetUrl(menuIndex), "http://", 7) == 0;
 }
@@ -534,7 +563,7 @@ void populateStreams(void) {
   Serial.println("Populating stream array from storage");
   // If prefs doesn't exist, this will throw a bunch of 'not found' errors.
   // Initialize the prefs storage by pulling STREAM_PIN low at boot.
-  for (int item=0; item < totalItems; item++) {
+  for (int item=0; item < TOTAL_ITEMS; item++) {
     prefs.begin(stream_item[item], true); // true = read only
     streamsPut(item, prefs.getString(stream_type[TYPE_TAG]).c_str(), 
                      prefs.getString(stream_type[TYPE_URL]).c_str());
@@ -550,7 +579,7 @@ void populateStreams(void) {
 
 // Fill the prefs object with data from the streamsX array
 void populatePrefs(void) {
-  for (int item=0; item < totalItems; item++) {
+  for (int item=0; item < TOTAL_ITEMS; item++) {
     prefs.begin(stream_item[item], false); // false = not readonly
     prefs.clear();
     prefs.putString(stream_type[TYPE_TAG], streamsGetTag(item));
@@ -583,7 +612,7 @@ void initializeStreams(void) {
 
   // 25 Streams (50 char name + 50 char url)
   char stream_data[50][50] = { // 50 elements of 50 chars
-    // 1-10, default streams, 25 totalItems
+    // 1-10, default streams, 25 total items
     "Psyndora Chillout","http://cast.magicstreams.gr:9125/stream",
     "Radio Bloodstream","http://uk1.internet-radio.com:8294/live.m3u",
     "Radio Play Emotions", "http://5.39.82.157:8054/stream",
@@ -613,8 +642,8 @@ void initializeStreams(void) {
     "Mr. Liberty Show","http://198.178.123.5:8258"
   };
 
-  for (int item=0; item < totalItems; item++) {
-    // stuff the prefs object
+  for (int item=0; item < TOTAL_ITEMS; item++) {
+    // stuff the prefs object with default data
     prefs.begin(stream_item[item], false); // false = read/write
     prefs.clear();
     prefs.putString(stream_type[TYPE_TAG], stream_data[item*2]);
