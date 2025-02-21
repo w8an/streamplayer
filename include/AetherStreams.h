dc850eb32b6f6b75ca2e04cc0fc4c1e882f1841b
Steven Stuart <w8an@users.noreply.github.com>
2025-02-21 17:09:35 -0500

    Separate default streams

---
+++ include/AetherStreams.h
@@ -0,0 +1,114 @@
+/*
+ * AetherStreams.h
+ * Default streaming station addresses
+ * 21-Feb-2025
+ */
+#define STREAMTAG_1 "Psyndora Chillout"
+#define STREAMURL_1 "http://cast.magicstreams.gr:9125"
+
+#define STREAMTAG_2 "Psyndora Psytrance"
+#define STREAMURL_2 "http://cast.magicstreams.gr:9111"
+
+#define STREAMTAG_3 "Radio Play Emotions"
+#define STREAMURL_3 "http://5.39.82.157:8054/stream"
+
+#define STREAMTAG_4 "Rare 80s Music"
+#define STREAMURL_4 "http://209.9.238.4:9844/"
+
+#define STREAMTAG_5 "Simply Oldies"
+#define STREAMURL_5 "http://uk5.internet-radio.com:8153"
+
+#define STREAMTAG_6 "Spirit Country Radio"
+#define STREAMURL_6 "http://us2.internet-radio.com:8035"
+
+#define STREAMTAG_7 "Synphaera Radio"
+#define STREAMURL_7 "http://ice2.somafm.com/synphaera-128-mp3"
+
+#define STREAMTAG_8 "The Seagull"
+#define STREAMURL_8 "http://us5.internet-radio.com:8121"
+
+#define STREAMTAG_9 "The Zone"
+#define STREAMURL_9 "http://uk1.internet-radio.com:8355"
+
+#define STREAMTAG_10 "XRDS.fm"
+#define STREAMURL_10 "http://us1.internet-radio.com:8321"
+
+#define STREAMTAG_11 "Ambient Radio"
+#define STREAMURL_11 "http://uk2.internet-radio.com:8171/stream"
+
+#define STREAMTAG_12 "Best of Art Bell"
+#define STREAMURL_12 "http://108.161.128.117:8050"
+
+#define STREAMTAG_13 "Big 80s Station"
+#define STREAMURL_13 "http://158.69.114.190:8024"
+
+#define STREAMTAG_14 "Big Hair Radio"
+#define STREAMURL_14 "http://192.111.140.11:8508"
+
+#define STREAMTAG_15 "Classical Radio"
+#define STREAMURL_15 "http://philae.shoutca.st:8204"
+
+#define STREAMTAG_16 "Dark Edge Radio"
+#define STREAMURL_16 "http://5.35.214.196:8000"
+
+#define STREAMTAG_17 "Detroit Industrial Underground"
+#define STREAMURL_17 "http://138.197.0.4:8000/stream"
+
+#define STREAMTAG_18 "Dimensione Relax"
+#define STREAMURL_18 "http://51.161.115.200:8012/stream"
+
+#define STREAMTAG_19 "Disco Funk"
+#define STREAMURL_19 "http://eu10.fastcast4u.com:8120"
+
+#define STREAMTAG_20 "EarthSong Experimental"
+#define STREAMURL_20 "http://cast3.my-control-panel.com:7084/autodj"
+
+#define STREAMTAG_21 "Ethereal Radio"
+#define STREAMURL_21 "http://us4.internet-radio.com:8073/live"
+
+#define STREAMTAG_22 "First Amendment Radio"
+#define STREAMURL_22 "http://198.178.123.8:7862"
+
+#define STREAMTAG_23 "Gothville"
+#define STREAMURL_23 "http://gothville.radio:8000/stream"
+
+#define STREAMTAG_24 "Hanks Old Time Radio"
+#define STREAMURL_24 "http://46.105.125.110:9714"
+
+#define STREAMTAG_25 "HardTecho and Schranz"
+#define STREAMURL_25 "http://schranz.in:8000"
+
+#define STREAMTAG_26 "Hit List Radio"
+#define STREAMURL_26 "http://us1.internet-radio.com:8118"
+
+#define STREAMTAG_27 "J-Pop Sakura"
+#define STREAMURL_27 "http://cast1.torontocast.com:2170"
+
+#define STREAMTAG_28 "Lounge Radio"
+#define STREAMURL_28 "http://fr1.streamhosting.ch:80/lounge128.mp3"
+
+#define STREAMTAG_29 "Mangled Web Radio"
+#define STREAMURL_29 "http://144.126.151.19:8000/mp3"
+
+#define STREAMTAG_30 "Megaton Cafe Radio"
+#define STREAMURL_30 "http://us2.internet-radio.com:8443"
+
+#define STREAMTAG_31 "Metal Express Radio"
+#define STREAMURL_31 "http://5.135.154.69:11590"
+
+#define STREAMTAG_32 "Metal Rock Radio"
+#define STREAMURL_32 "http://kathy.torontocast.com:2800"
+
+#define STREAMTAG_33 "Mission Control Radio"
+#define STREAMURL_33 "http://151.80.42.191:8372"
+
+#define STREAMTAG_34 "Moon Mission Recordings"
+#define STREAMURL_34 "http://uk5.internet-radio.com:8306"
+
+#define STREAMTAG_35 "Move Da House"
+#define STREAMURL_35 "http://uk7.internet-radio.com:8000"
+
+#define STREAMTAG_36 "Mr. Liberty Show"
+#define STREAMURL_36 "http://198.178.123.5:8258"
+
+// eof
