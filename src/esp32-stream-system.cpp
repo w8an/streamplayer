db7d0428ddba3824b4897bd6e6ff40a264f6b142
Steven Stuart <w8an@users.noreply.github.com>
2025-06-05 09:35:13 -0400

    documentation updates

--- src/esp32-stream-system.cpp
+++ src/esp32-stream-system.cpp
@@ -17,7 +17,23 @@
  * Hold STREAM_PIN low on reset to load and store default stream data.
  * Hold META_PIN low during operation to suppress meta data display.
  * Hold NVS_CLR_PIN low on reset to erase memory content (factory reset).
- */
+ */ 
+
+/*
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 
 #include <Arduino.h>
 #include <Preferences.h>
